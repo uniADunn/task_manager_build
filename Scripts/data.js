60dd733667093f2856bfd108f54d64ebba97a597
uniADunn <adunn300@caledonian.ac.uk>
2026-05-10 12:56:31 +0100

    Implement task validation and task addition functionality in data.js. Added a function to retrieve tasks.

---
+++ Scripts/data.js
@@ -0,0 +1,53 @@
+const tasks = [];
+const validStatuses = ['Not Started', 'In Progress', 'Completed'];
+
+const validateText = (text) =>{
+    if(text){
+        if(!text.match(/[^a-zA-Z0-9 ]/)){            
+            //console.log(`text validated: ${text}`);
+            return true;
+        }
+        else{
+            throw new Error('text can only contain letters, numbers, spaces.');            
+        }
+    }
+    else{
+        throw new Error('text cannot be empty.');
+    }
+}
+const validateStatus = (status) => {
+    if(validStatuses.includes(status)){
+        return true;
+    }
+    else{
+        throw new Error('Invalid status.');
+    }
+}
+
+function addTask(title, description, status){
+    try{
+        validateText(title);
+        console.log('title validated');
+        validateText(description);
+        console.log('description validated');
+        validateStatus(status);
+        console.log('status validated');
+        const newTask = new Task(title, description, status);
+        tasks.push(newTask);
+        return true;
+        
+    } catch (error) {
+        console.error(error.message);
+        return false;
+    }
+};
+
+function getTasks(){
+    if(tasks.length === 0){
+        console.log('No tasks found.');
+        return null;
+    }
+    else{
+        return tasks;
+    }
+}
