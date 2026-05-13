1e917378574d6d34ad761e48b918bf50912da6fd
uniADunn <adunn300@caledonian.ac.uk>
2026-05-13 14:29:50 +0100

    Refactor task rendering in render.js for improved structure and styling; moved all unessassary css rules from render.js and updated css styles.

--- Scripts/render.js
+++ Scripts/render.js
@@ -1,36 +1,97 @@
 // render.js handles all DOM manipulation related to rendering tasks on the page,
 // including creating task elements, updating task status colours and displaying messages when there are no tasks to display.
 
-const tasklist = document.querySelector('#task-container');// container element for task list
-
+const taskContainer = document.querySelector('#task-container');
 // function to create and display "No Tasks to display" message when there are no tasks in the task list.
 noTasksMessage = ()=>{
     //create and append message element to task list container
     const p = document.createElement('p');
     p.id = 'no-tasks';
     p.textContent = 'No Tasks To Display';
-    tasklist.appendChild(p);
+    taskContainer.appendChild(p);
 }
 
 //function to remove existing tasks or "no tasks to display" message from the DOM before rendering updated task list
 removeMessage = () => {
     //check if no task message is present in the dom and remove it.
-    if(tasklist.contains(document.querySelector('#no-tasks'))){
+    if(taskContainer.contains(document.querySelector('#no-tasks'))){
         console.log('Removing existing "No Tasks To Display" message from DOM...');
-        tasklist.removeChild(document.querySelector('#no-tasks'));
+        taskContainer.removeChild(document.querySelector('#no-tasks'));
     }
     // if there are existing tasks in the DOM, remove them before rendering updated task list
-    else if(tasklist.contains(document.querySelector('.task'))){
+    else if(taskContainer.contains(document.querySelector('.task'))){
         console.log('Removing existing tasks from DOM...');
         const existingTasks = document.querySelectorAll('.task');
         // remove each existing task element from the DOM
         existingTasks.forEach(
             (task) =>{
-                tasklist.removeChild(task);
+                taskContainer.removeChild(task);
             });
     }
 }
 
+createTaskElement = (task, index, elementTag,  className) => {    
+    //create task container element and data attribute for task index
+    const element = document.createElement(elementTag);
+    //if element is a label for created at or description, add specific class name for styling
+    
+    if(className){
+        element.classList.add(className);
+        
+    }
+    if(elementTag === 'select'){
+        element.name = 'task-status';
+    }
+    element.dataset.index = index;
+    return element;
+}
+
+insertTaskContent = (element, content) =>{
+    if(element.classList.contains('task-created-at')){
+        const label_p = document.createElement('p');
+        label_p.innerHTML = '<i>Created At:</i>';
+        
+        const content_p = document.createElement('p');
+        content_p.innerHTML = `<br>&emsp;<b>${content}</b></br>`;
+        
+        element.innerHTML = `${label_p.innerHTML} ${content_p.innerHTML}`;
+    }
+    else if(element.classList.contains('task-description')){
+        const label_p = document.createElement('p');
+        label_p.innerHTML = '<i>Description:</i>';
+        
+        const content_p = document.createElement('p');
+        content_p.innerHTML = `<br>&emsp;<b>${content}</b></br>`;
+        
+        element.innerHTML = `${label_p.innerHTML} ${content_p.innerHTML}`;
+    }
+    else{
+        element.textContent = content;
+    }
+}
+createOptionsForSelectElement = (selectElement, validStatuses) =>{
+    validStatuses.forEach(
+        (statusOption) =>{
+            const opt = document.createElement('option');
+            opt.classList.add('status-option');
+            opt.value = statusOption;
+            opt.textContent = statusOption;            
+            selectElement.appendChild(opt);
+        });
+}
+
+setCurrentTaskStatus = (selectElement, currentStatus) =>{
+    selectElement.value = currentStatus;
+    if(selectElement.value === 'Not Started'){
+        selectElement.style.backgroundColor = '#911818';
+    }
+    else if(selectElement.value === 'In Progress'){
+        selectElement.style.backgroundColor = '#c8af0d';
+    }
+    else if(selectElement.value === 'Completed'){
+        selectElement.style.backgroundColor = '#227022';
+    }
+}
 //function to create and append task elements to the DOM based on the current task list
 //each task element includes the task title, description, status dropdown and delete button
 //task status dropdown is dynamically generated based on valid statuses defined in data.js and is pre-selected to the current task status
@@ -39,98 +100,43 @@ loadTasks = (tasks) => {
     tasks.forEach(
             (task, index) => {
                 //task container
-                const taskDiv = document.createElement('div');
-                taskDiv.classList.add('task');
-                taskDiv.dataset.index = index;
-                taskDiv.style.backgroundColor = '#0B0D67';
+                const taskDiv = createTaskElement(task, index, 'div', 'task');
+                //details and actions container
+                const detailsAndActionsContainer = createTaskElement(task, index, 'div', 'details-and-actions');
                 //seperator containers
-                const divSeperatorTop = document.createElement('div');
-                divSeperatorTop.classList.add('separatorTop');
-                taskDiv.appendChild(divSeperatorTop);
+                const divSeparatorTop = createTaskElement(task, index, 'div', 'separatorTop');
+                const divSeparatorLeft = createTaskElement(task, index, 'div', 'separatorLeft');
+                const divSeparatorRight = createTaskElement(task, index, 'div', 'separatorRight');
+                //content elements
+                const createdAtElement = createTaskElement(task, index, 'p', 'task-created-at');
+                const titleElement = createTaskElement(task, index, 'h3', 'task-title');
+                const descriptionElement = createTaskElement(task, index, 'p', 'task-description');
+                //action elements
+                const selectElement = createTaskElement(task, index, 'select', 'task-status');
+                const deleteBtnElement = createTaskElement(task, index, 'button', 'deleteBtn');
 
-                //task content
-                const title = document.createElement('h3');
-                title.textContent = `${task.title.toUpperCase()}`;
-                taskDiv.appendChild(divSeperatorTop);
-                divSeperatorTop.appendChild(title);
+                insertTaskContent(titleElement, task.title.toUpperCase());
+                insertTaskContent(createdAtElement, task.start_ts);
+                insertTaskContent(descriptionElement, task.description);
+                insertTaskContent(deleteBtnElement, 'Delete');
 
-                const divDetailsAndActionsContainer = document.createElement('div');
-                divDetailsAndActionsContainer.classList.add('details-and-actions');
-                taskDiv.appendChild(divDetailsAndActionsContainer);
-
-                
-                const divSeperatorLeft = document.createElement('div');
-                divSeperatorLeft.classList.add('separatorLeft');
-                taskDiv.appendChild(divSeperatorLeft);
-
-                const divSeperatorRight = document.createElement('div');
-                divSeperatorRight.classList.add('separatorRight');
-                taskDiv.appendChild(divSeperatorRight);
-
-                
-                //description element with italicized "Description:" label and line break before description text
-                const description = document.createElement('p');
-                description.innerHTML = `<span style="font-style:italic;">Description:</span><br>&emsp; ${task.description}.`;
-
-                const createdAt = document.createElement('p');
-                createdAt.innerHTML = `<span style="font-style:italic;">Created At:</span> ${task.start_ts}.`;
-
-                //select element for task status with options generated from valid statuses defined in data.js and pre-selected to current task status
-                const select = document.createElement('select');
-                select.name = 'update-status';
-                select.style.backgroundColor = '#0B0D67';                
-                select.style.padding = '10px';
-                select.style.border = 'none';
-                select.style.color = '#FFFFFF';
-                select.classList.add('task-status');
-                select.dataset.index = index;
-                //get statuses from data.js and create option element for each status, appending them to the select element
+                //create select options for task status dropdown based on valid statuses defined in data.js
                 const validStatuses = getStatuses();
-                validStatuses.forEach(
-                    (statusOption) =>{
-                        const opt = document.createElement('option');
-                        opt.value = statusOption;
-                        opt.textContent = statusOption;
-                        opt.style.backgroundColor = '#FFFFFF';
-                        opt.style.color= '#000000';
-                        opt.style.margin = '10px';
-                        opt.style.textAlign = 'center';
-                        select.appendChild(opt);
-                    });
-                    //set select element value to current task status and update background colour based on status
-                    select.value = task.status;
-                    if(select.value === 'Not Started'){
-                        select.style.backgroundColor = '#911818';
-                    }
-                    else if(select.value === 'In Progress'){
-                        select.style.backgroundColor = '#c8af0d';
-                    }
-                    else if(select.value === 'Completed'){
-                        select.style.backgroundColor = '#227022';
-                    }
-                //delete button element with styling and data attribute for task index
-                const deleteBtn = document.createElement('button');
-                deleteBtn.textContent = 'Delete';
-                deleteBtn.classList.add('deleteBtn');
-                deleteBtn.style.backgroundColor = '#911818';
-                deleteBtn.style.color = '#FFFFFF';
-                deleteBtn.style.padding = '10px 30px';
-                deleteBtn.style.border = 'none';
-                deleteBtn.style.borderRadius = '5px';
-                
-                deleteBtn.style.cursor = 'pointer';
-                deleteBtn.dataset.index = index;
+                createOptionsForSelectElement(selectElement, validStatuses);
+                //set select element value to current task status and update background colour based on status
+                setCurrentTaskStatus(selectElement, task.status);
+                    
+                taskContainer.appendChild(taskDiv);
+                taskDiv.appendChild(divSeparatorTop);
+                taskDiv.appendChild(detailsAndActionsContainer);
+                detailsAndActionsContainer.appendChild(divSeparatorLeft);
+                detailsAndActionsContainer.appendChild(divSeparatorRight);
+                divSeparatorTop.appendChild(titleElement);
+                divSeparatorLeft.appendChild(createdAtElement);
+                divSeparatorLeft.appendChild(descriptionElement);
+                divSeparatorRight.appendChild(selectElement);   
+                divSeparatorRight.appendChild(deleteBtnElement);             
 
-                //append task content elements to their respective separator containers and append separator containers to task container,
-                //then append task container to task list container in the DOM
-                
-                divSeperatorLeft.appendChild(createdAt);
-                divSeperatorLeft.appendChild(description);
-                divSeperatorRight.appendChild(select);
-                divSeperatorRight.appendChild(deleteBtn);
-                divDetailsAndActionsContainer.appendChild(divSeperatorLeft);
-                divDetailsAndActionsContainer.appendChild(divSeperatorRight);
-                tasklist.appendChild(taskDiv);
             });
 }
 
