ce9094577c114c433d0e9c708574467c6cb6ca37
uniADunn <adunn300@caledonian.ac.uk>
2026-05-19 16:38:51 +0100

    Add test case for validating task start timestamp format

--- tests/task.test.js
+++ tests/task.test.js
@@ -16,4 +16,8 @@ test('Task is created with Correct Status', () => {
     const task = new Task('Test Task', 'this is a test task', 'Not Started');
     expect(statuses).toContain(task.status);
 });
+test('Task start_ts is a valid date string', () => {
+    const task = new Task('Buy milk', 'Get 2 pints', 'pending');
+    expect(task.start_ts).toMatch(/^\d{2}\/\d{2}\/\d{4}$/);
+});
 
